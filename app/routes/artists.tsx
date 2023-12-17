3fb21735b24f45bdc4cd9b025f29ab8655e785a7
Antoine Labard <antoine@labard.fr>
2023-12-17 20:44:50 +0100

    💄 style artists.tsx

--- app/routes/artists.tsx
+++ app/routes/artists.tsx
@@ -41,28 +41,9 @@ export default function ArtistsPage() {
     return (
         <div>
             <Header/>
-            <main>
-                <h1>Welcome to Remix</h1>
-                <Link to="/artist/new">Add a new artist</Link>
-                <fieldset>
-                    <legend>Filter by genres</legend>
-                    {genres.map((genre) => (
-                        <label key={genre} htmlFor={genre}>
-                            <input type="checkbox" id={genre} name="filteredGenres"
-                                   onClick={() => toggleFilteredGenre(genre)}/>{genre}
-                        </label>
-                    ))}
-                </fieldset>
-                <fieldset>
-                    <legend>Filter by instruments</legend>
-                    {instruments.map((instrument) => (
-                        <label key={instrument} htmlFor={instrument}>
-                            <input type="checkbox" id={instrument} name="filteredInstruments"
-                                   onClick={() => toggleFilteredInstrument(instrument)}/>{instrument}
-                        </label>
-                    ))}
-                </fieldset>
-                <div>
+            <main className="p-5">
+                <h1 className="text-6xl font-bold text-center m-5">Welcome to Remix</h1>
+                <div className="flex flex-row justify-between">
                     <ul>{artists
                         .filter((artist: Artist) =>
                             filteredGenres.length === 0
@@ -74,7 +55,32 @@ export default function ArtistsPage() {
                             <li key={artist.id}>
                                 <ArtistCard artist={artist}/>
                             </li>
-                        ))}</ul>
+                        ))}
+                    </ul>
+                    <div className="flex flex-col">
+                        <Link className="m-3 px-5 py-2 w-fit bg-orange-400 rounded-lg font-bold" to="/artist/new">Add a new
+                            artist</Link>
+                        <div className="flex flex-row">
+                            <fieldset className="m-3 border-4 rounded-lg border-orange-400 flex flex-col w-fit p-3">
+                                <legend>Filter by genres</legend>
+                                {genres.map((genre) => (
+                                    <label key={genre} htmlFor={genre}>
+                                        <input type="checkbox" id={genre} name="filteredGenres"
+                                               onClick={() => toggleFilteredGenre(genre)}/>{genre}
+                                    </label>
+                                ))}
+                            </fieldset>
+                            <fieldset className="m-3 border-4 rounded-lg border-orange-400 flex flex-col w-fit p-3">
+                                <legend>Filter by instruments</legend>
+                                {instruments.map((instrument) => (
+                                    <label key={instrument} htmlFor={instrument}>
+                                        <input type="checkbox" id={instrument} name="filteredInstruments"
+                                               onClick={() => toggleFilteredInstrument(instrument)}/>{instrument}
+                                    </label>
+                                ))}
+                            </fieldset>
+                        </div>
+                    </div>
                 </div>
             </main>
             <Footer/>
