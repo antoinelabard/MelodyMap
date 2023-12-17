5e971fc3aa41e3230a32b262f5f5dec97c103d27
Antoine Labard <antoine@labard.fr>
2023-12-17 20:19:41 +0100

    💄 style ConcertCard.tsx

--- app/components/ConcertCard.tsx
+++ app/components/ConcertCard.tsx
@@ -7,11 +7,11 @@ export function ConcertCard({concert}) {
             throw new Error("No concert prop provided")
         }
     })
-    return (<div>
-            <p>{new Date(concert.datetime).toString()}</p>
+    return (<div className="border-2 rounded m-5 p-5 w-96 flex flex-col align-middle">
+            <p className="font-bold">{new Date(concert.datetime).toString()}</p>
             <p>{concert.location.address}, {concert.location.city}</p>
-            <p>{concert.genre}</p>
-            <Link to={`concert/${concert.id}`}>Modify</Link>
+            <p className="py-1 px-4 m-1 rounded-full border-2 border-orange-400 text-center w-fit">{concert.genre}</p>
+            <Link className="m-3 px-5 py-2 bg-orange-400 rounded-lg font-bold text-center" to={`concert/${concert.id}`}>Modify</Link>
         </div>
     )
 }
