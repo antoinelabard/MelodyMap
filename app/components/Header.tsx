fea82f56a1a261cb13239b4ab7bdcbbdfa93c510
Antoine Labard <antoine@labard.fr>
2023-12-17 19:52:37 +0100

    💄 style Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,11 @@
 import {Link} from "@remix-run/react";
 
 export function Header() {
-    return (<header>
-            <p>Remix</p>
+    return (<header className="flex flex-row justify-between items-center bg-stone-900 p-10">
+            <Link to="/" className="text-4xl text-orange-50 font-bold">Remix</Link>
         <nav>
-            <Link to={`/artists`}>Artists</Link>
-            <Link to={`/concerts`}>Concerts</Link>
+            <Link to={`/artists`} className="m-3 px-5 py-2 bg-orange-400 rounded-lg font-bold">Artists</Link>
+            <Link to={`/concerts`} className="m-3 px-5 py-2 bg-orange-400 rounded-lg font-bold">Concerts</Link>
         </nav></header>
     )
 }
