cb59e58d29d3586eabc9dbdfe76f7dda96f8f149
Antoine Labard <antoine@labard.fr>
2023-12-17 20:11:12 +0100

    💄 style ArtistCard.tsx

--- app/components/ArtistCard.tsx
+++ app/components/ArtistCard.tsx
@@ -7,15 +7,15 @@ export function ArtistCard({artist}) {
             throw new Error("No artist prop provided")
         }
     })
-    return (<div>
-            <p>{artist.name}</p>
-            <ul>{artist.genres.map((genre) => (
-                <li key={genre}>{genre}</li>
+    return (<div className="border-2 rounded m-5 p-5 w-80 flex flex-col align-middle">
+            <p className="text-xxl font-bold m-2 underline">{artist.name}</p>
+            <ul className="flex flex-row flex-wrap">{artist.genres.map((genre) => (
+                <li className="py-1 px-4 m-1 rounded-full border-2 border-orange-400" key={genre}>{genre}</li>
             ))}</ul>
-            <ul>{artist.instruments.map((instrument) => (
-                <li key={instrument}>{instrument}</li>
+            <ul className="flex flex-row flex-wrap">{artist.instruments.map((instrument) => (
+                <li className="py-1 px-4 m-1 rounded-full border-2 border-orange-400" key={instrument}>{instrument}</li>
             ))}</ul>
-            <Link to={`artist/${artist.id}`}>Modify</Link>
+            <Link className="m-3 px-5 py-2 bg-orange-400 rounded-lg font-bold text-center" to={`artist/${artist.id}`}>Modify</Link>
         </div>
     )
 }
